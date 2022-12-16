e464881df7f81c47a8f39d3aa3dca788867eae31
danmatuoka <dan_mtk@hotmail.com>
2022-12-16 19:53:23 -0300

    finished task 1

--- albums/serializers.py
+++ albums/serializers.py
@@ -3,16 +3,6 @@ from rest_framework import serializers
 from .models import Album
 
 
-""" class AlbumSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    year = serializers.IntegerField()
-    user_id = serializers.IntegerField(read_only=True)
-
-    def create(self, validated_data):
-        return Album.objects.create(**validated_data) """
-
-
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
--- users/serializers.py
+++ users/serializers.py
@@ -3,38 +3,20 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-""" class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message="A user with that username already exists.",
-            )
-        ],
-    )
-    email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
-    )
-    password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    is_superuser = serializers.BooleanField(read_only=True)
-
-    def create(self, validated_data: dict) -> User:
-        return User.objects.create_superuser(**validated_data)
-
-    def update(self, instance: User, validated_data: dict) -> User:
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-
-        instance.save()
-
-        return instance """
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email", "password", "first_name", "last_name", "is_superuser"]
         extra_kwargs = {"password": {"write_only": True}}
+
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+
+
+    def validate_email(self, value: str):
+        try:
+            User.objects.get(email=value)
+            raise serializers.ValidationError("This field must be unique.")
+        except User.DoesNotExist:
+            return value
