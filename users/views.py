62ec2e1060dd99fa3672a088c4e62960b8aeccc1
danmatuoka <dan_mtk@hotmail.com>
2022-12-17 18:07:37 -0300

    add createApiView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView, Request, Response, status
+from rest_framework.generics import CreateAPIView
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
@@ -6,18 +7,9 @@ from django.shortcuts import get_object_or_404
 from .permissions import IsAccountOwner
 
 
-class UserView(APIView):
-    def post(self, request: Request) -> Response:
-        """
-        Registro de usuários
-        """
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        serializer.save()
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
-
+class UserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class UserDetailView(APIView):
     authentication_classes = [JWTAuthentication]
